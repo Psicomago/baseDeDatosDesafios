ae0b04e6b1271319944a96e46f275cdc97cdd8ca
Carlos Aravena <carlosaravena.coach@gmail.com>
2022-05-31 21:20:25 -0400

    ultima actualizacion

--- evaluados/top100/script.sql
+++ evaluados/top100/script.sql
@@ -8,13 +8,14 @@
     DROP DATABASE peliculas;
 -- 1. Crear una base de datos llamada películas. (1 Punto)
     CREATE DATABASE peliculas;
-    \c peliculas -- conecta a base de datos peliculas
+    -- conecta a base de datos peliculas
+    \c peliculas 
 
     CREATE TABLE peliculas(
     id INT,
     pelicula VARCHAR(60),
-    estreno INT(04),
-    director VARCHAR(20),
+    estreno INT,
+    director VARCHAR(60),
     PRIMARY KEY(id)
     );
 
@@ -26,23 +27,23 @@
 
 
 -- 2. Cargar ambos archivos a su tabla correspondiente. (1 Punto)
-    \COPY peliculas FROM '../ApoyoTop100/peliculas.csv' csv header;
-    \COPY reparto FROM '../ApoyoTop100/reparto.csv' delimiter ',';
+    \COPY peliculas FROM 'download/peliculas.csv' csv header;
+    \COPY reparto FROM 'download/reparto.csv' delimiter ',';
 
 -- 3. Obtener el ID de la película “Titanic”. (1 Punto)
     SELECT id FROM peliculas WHERE pelicula = 'Titanic';
 
 
 -- 4. Listar a todos los actores que aparecen en la película "Titanic". (1 Puntos)
-    SELECT actor FROM reparto WHERE id_pelicula = (SELECT id FROM peliculas WHERE pelicula = 'Titanic');
+    SELECT actor AS elenco_titanic FROM reparto WHERE id_pelicula = (SELECT id FROM peliculas WHERE pelicula = 'Titanic');
 
 -- 5. Consultar en cuántas películas del top 100 participa Harrison Ford. (2 Puntos)
-    SELECT COUNT(id_pelicula) FROM reparto WHERE actor = 'Harrison Ford';
+    SELECT COUNT(id_pelicula) AS top100_harrison_ford FROM reparto WHERE actor = 'Harrison Ford';
 
 
 -- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de manera ascendente. (1 punto)
 
-    SELECT pelicula FROM peliculas WHERE estreno BETWEEN 1990 AND 1999 ORDER BY pelicula ASC;
+    SELECT pelicula AS estrenos_entre_1990_y_1999 FROM peliculas WHERE estreno BETWEEN 1990 AND 1999 ORDER BY pelicula ASC;
 
 -- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
 
@@ -52,4 +53,4 @@
 
 -- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas. (2 punto)
 
-    SELECT MAX(LENGTH(pelicula)) FROM peliculas;
+    SELECT MAX(LENGTH(pelicula)) AS longuitud_maxima FROM peliculas;
